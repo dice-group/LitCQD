fd8dfc2512a0ba720682cf62aa189f302922397f
renzhonglu11 <lu631992923q@gmail.com>
2023-06-11 12:46:50 +0000

    update script

--- final_scripts/final_script.sh
+++ final_scripts/final_script.sh
@@ -14,7 +14,3 @@ python3 eval_cqd.py --cuda --do_test --checkpoint_path checkpoints_FB15K-237/sub
 
 python3 main.py --cuda --do_train --do_test --data_path data/scripts/generated/FB15K-237_dummy_kblrn --use_attributes -n 0 --rank 1000 -lr 0.1 --attr_loss mae --alpha 0.5 --geo cqd-complexa --batch_size 1024 --test_batch_size 100 --train_times 100 --valid_epochs 10 --print_on_screen
 
-
-python3 eval_cqd.py --do_test --checkpoint_path checkpoints_FB15K-237/submitted_paper --data_path data/scripts/generated/FB15K-237_dummy_kblrn --use_attributes --rank 1000 --geo cqd-complexa --test_batch_size 1024 --print_on_screen
-
-python3 eval_cqd.py --do_test --checkpoint_path checkpoints_FB15K-237/submitted_paper --data_path data/FB15k-237-q2b --rank 1000 --geo cqd-complexa --test_batch_size 1024 --print_on_screen
